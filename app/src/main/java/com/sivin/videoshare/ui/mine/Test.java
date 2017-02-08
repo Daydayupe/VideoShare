ef44db9827af10dbbfb08ae9acc26adb14e9af8d
wangbin <759365392@qq.com>
2017-02-08 18:28:25 +0800

    测试

---
+++ app/src/main/java/com/sivin/videoshare/ui/mine/Test.java
@@ -0,0 +1,14 @@
+package com.sivin.videoshare.ui.mine;
+
+/**
+ * <br/>************************************************
+ * <br/>PROJECT_NAME : VideoSharew
+ * <br/>PACKAGE_NAME : com.sivin.videoshare.ui.mine
+ * <br/>AUTHOR : WangBin
+ * <br/>DATA : 17/2/8
+ * <br/>TIME : 下午6:27
+ * <br/>MSG :
+ * <br/>************************************************
+ */
+public class Test {
+}
